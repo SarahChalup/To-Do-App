71c2557530cfe59829a4a1c05120046e5cd3f17d
Sarah Chalup <a2018112520@estudiantes.upsa.edu.bo>
2022-08-04 13:23:22 -0400

    Creando un High order component de pruebita en

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+
+function App(props){
+    return(
+        <h1>{props.greeting}, {props.name}</h1>
+    )
+}
+
+function withGreeting(WrappedComponent){
+    return function WrappedComponentWithGreeting(greeting){
+        return function Component(props){
+            return(
+                <React.Fragment>
+                    <WrappedComponent {...props} greeting={greeting} />
+                    <p>Estamos acompañando al wrapped component</p>
+                </React.Fragment>
+            )
+        }
+    }
+}
+
+const AppWithGreeting = withGreeting(App)("Buenas tardes");
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(  
-    <App />
+    <AppWithGreeting
+        name="Sari" />
 );
 
